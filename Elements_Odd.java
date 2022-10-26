43d0da297c84b173807a7e380941161a66a76eb1
SanketBezalwar <111625762+SanketBezalwar@users.noreply.github.com>
2022-10-26 10:31:59 +0530

    Day 7

---
+++ Elements_Odd.java
@@ -0,0 +1,20 @@
+
+public class Elements_Odd {
+
+	public static void main(String[] args) {
+
+		int[] num = new int[] { 11, 32, 41, 58, 47, 63, 20, 33, 74 };
+
+		System.out.println("Elements : 11 32 41 58 47 63 20 33 74 ");
+		
+		System.out.println();
+
+		System.out.println("Array Elements on Odd Positions are : ");
+
+		for (int i = 0; i < num.length; i = i + 2) {
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
